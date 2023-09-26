4ab570f3a36f580740f6df80fd54dd885061a03f
MacReiben <macreiben@gmail.com>
2023-09-26 11:44:51 +0200

    gros commit tout moche mais tu as les tests ;)

--- src/services/SomeDummyService.js
+++ src/services/SomeDummyService.js
@@ -1,12 +1,19 @@
-
 class SomeDummyService {
-    doSomething(left, right) {
-        return left + right;
-    }
+	doSomething(left, right) {
+		return left + right;
+	}
 
-    someUntestedMethod() {
-        return "som_data"
-    }
+	someUntestedMethod() {
+		return "som_data";
+	}
+
+	fizzBuzzIter1(input) {
+		if (input % 5 == 0) {
+			return "Buzz";
+		}
+
+		return "Fizz";
+	}
 }
 
-export default SomeDummyService
+export default SomeDummyService;
--- src/tests/some_tests.spec.js
+++ src/tests/some_tests.spec.js
@@ -1,15 +1,55 @@
 import SomeDummyService from "../services/SomeDummyService";
 
-describe('Some test suite', () => {
-    test('Some unit test here', () => {
-        expect(1).toBe(1);
-    })
+describe("Some test suite", () => {
+	/* Write a fizzBuzzIter1 method that take an integer as an input and return:
 
-    test('Some unit test that access a service', () => {
-        const target = new SomeDummyService()
+    If the input is divisible by 3, return Fizz
+    If the input is divisible by 5, return Buzz
+    If the input is divisible by 3 and 5, return FizzBuzz
+    Else return the input
+ */
 
-        const actual = target.doSomething(1, 2)
+	test("If the input is 6 then return Fizz", () => {
+		// ARRANGE
+		const target = new SomeDummyService();
 
-        expect(actual).toBe(3)
-    })
-})
+		// ACT
+		const actual = target.fizzBuzzIter1(6);
+
+		// ASSERT
+		expect(actual).toBe("Fizz");
+	});
+
+	test("If the input is 5 then return Buzz", () => {
+		// ARRANGE
+		const target = new SomeDummyService();
+
+		// ACT
+		const actual = target.fizzBuzzIter1(5);
+
+		// ASSERT
+		expect(actual).toBe("Buzz");
+	});
+
+	test("If input is 10 then return Buzz", () => {
+		// ARRANGE
+		const target = new SomeDummyService();
+
+		// ACT
+		const actual = target.fizzBuzzIter1(10);
+
+		// ASSERT
+		expect(actual).toBe("Buzz");
+	});
+
+	test("If input is 25 then return Buzz", () => {
+		// ARRANGE
+		const target = new SomeDummyService();
+
+		// ACT
+		const actual = target.fizzBuzzIter1(25);
+
+		// ASSERT
+		expect(actual).toBe("Buzz");
+	});
+});
